e7c53e61845026b340652149db3167288abd6e29
smirklijie <871063850@qq.com>
2016-11-25 18:12:33 +0800

    一些bug的修改，模型文件的说明，debug模式无法打开opencv_world310d.lib，待解决

--- NativeEasyPR/easyprtest/main.cpp
+++ NativeEasyPR/easyprtest/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 #include "process.h"
 #include "easypr/core/plate_recognize.h"
 #include <opencv2/opencv.hpp>
@@ -6,13 +7,16 @@ using namespace cv;
 using namespace easypr;
 using namespace std;
 int main(){
-	string path = "D:/JAVA/workspaceneo/easyprjava-parent/easyprjava/src/test/resources/model";
-	Process pr(path);
+	//模型文件model 和province_mapping需放到一个文件夹下面，这里已经提取出来放到model文件下
+	//源程序没有处理加载失败情况，很是糟糕,path为自己的model文件路径
+	string path = "D:/workspace/git/EasyPR-native/model";
+	Process *pr=new Process(path);
 	
-	Mat src = imread("D:/Users/Administrator/Desktop/test.jpg");
-	string plate=pr.process(src);
+	Mat src = imread("D:/workspace/git/EasyPR-native/EasyPR/resources/image/test.jpg");
+	string plate=pr->process(src);
 	cout << plate << endl;
 	getchar();
+	delete pr;
 	return 0;
 
 }
